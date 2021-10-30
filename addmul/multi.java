8850c694771625155ce78ee13383e07e5796a445
hiteshh456 <91420226+hiteshh456@users.noreply.github.com>
2021-10-30 22:10:08 +0530

    Add files via upload
    assignment2

---
+++ addmul/multi.java
@@ -0,0 +1,34 @@
+package addmul;
+
+import java.util.Scanner;
+
+public class multi {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input first number: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Input second number: ");
+        int num2 = in.nextInt();
+
+        System.out.print("Input second number: ");
+        int num3 = in.nextInt();
+
+
+        System.out.println(num1 + " + " + num2 + " = " +
+                (num1 + num2));
+
+        System.out.println(num1 + " + " + num2 + " + " + num3 + " = " +
+                (num1 + num2 + num3));
+
+        System.out.println(num1 + " * " + num2 + " = " +
+                (num1 * num2));
+
+        System.out.println(num1 + " * " + num2 + " * " + num3 +  " = " +
+                (num1 * num2 * num3));
+
+    }
+
+}
